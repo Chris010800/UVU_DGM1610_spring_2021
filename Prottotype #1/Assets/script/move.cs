eec93e6c8af6ff85a3145360f92eb93d87823598
Chris <carhuatanta.cc@gmail.com>
2021-01-25 18:38:51 -0700

    added Movement Script and the Camera

--- Prottotype #1/Assets/script/move.cs
+++ Prottotype #1/Assets/script/move.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 {
+    // Access Modifier, Data Type, Name
+    public float speed = 5.0f;
+    public float hInput;
+    public float fInput;
+
+    /// </summary>
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,10 @@ public class move : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        hInput = Input.GetAxis("Horizontal");
+        fInput = Input.GetAxis("Vertical");
+       
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * fInput);
+        transform.Translate(Vector3.right * Time.deltaTime * speed * hInput);
     }
 }
