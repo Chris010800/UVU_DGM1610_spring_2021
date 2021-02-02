d248a75c1eece1b650cf03b0f3a2d752d3acb33d
Chris <carhuatanta.cc@gmail.com>
2021-02-01 19:17:04 -0700

    Prototype #2

---
+++ Prototype #2/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public float hInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
+
+        if (transform.position.x < -15)
+        { 
+         transform.position = new Vector3(-15, transform.position.y, transform.position.z); 
+        }
+        if (transform.position.x > 15) 
+        { transform.position = new Vector3(15, transform.position.y, transform.position.z); }
+    }
+}
