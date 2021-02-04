f1343e6ddeb75b77636a91306dab5b8ef309edc4
Chris <carhuatanta.cc@gmail.com>
2021-02-03 18:29:53 -0700

    lesson 2.3

--- Prototype #2/Assets/Scripts/Player.cs
+++ Prototype #2/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     public float speed = 10.0f;
     public float hInput;
+    public float xRange = 15;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +20,15 @@ public class Player : MonoBehaviour
         hInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * hInput * Time.deltaTime * speed);
 
-        if (transform.position.x < -15)
+        if (transform.position.x < -xRange)
         { 
-         transform.position = new Vector3(-15, transform.position.y, transform.position.z); 
+         transform.position = new Vector3(-xRange, transform.position.y, transform.position.z); 
         }
-        if (transform.position.x > 15) 
-        { transform.position = new Vector3(15, transform.position.y, transform.position.z); }
+        if (transform.position.x > xRange) 
+        { transform.position = new Vector3(xRange, transform.position.y, transform.position.z); }
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        { // launch projectile
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
