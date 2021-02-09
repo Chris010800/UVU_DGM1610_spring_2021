30c5d4948a4739d317f9ebf0125c744607a6f5d7
Chris <carhuatanta.cc@gmail.com>
2021-02-08 17:28:00 -0700

    collision

--- Prototype #2/Assets/Scripts/DestrroyOutofBonds.cs
+++ Prototype #2/Assets/Scripts/DestrroyOutofBonds.cs
@@ -22,6 +22,7 @@ public class DestrroyOutofBonds : MonoBehaviour
         }  
         else if(transform.position.z < lowerBound)
         {
+            
             Destroy(gameObject);
         }
     }
