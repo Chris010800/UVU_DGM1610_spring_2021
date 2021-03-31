6b1293ca52b614bf76e50d7ae15e49b4c785a07e
Chris <carhuatanta.cc@gmail.com>
2021-03-31 12:40:29 -0600

    Lesson 4.4

--- Prototype 4/Assets/Scripts/RotateCamera.cs
+++ Prototype 4/Assets/Scripts/RotateCamera.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotateCamera : MonoBehaviour
 {
-    public float rotationSpeed = 50;
+    private float rotationSpeed = 80;
     // Start is called before the first frame update
     void Start()
     {
--- Prototype 4/Assets/Scripts/SpawnManager.cs
+++ Prototype 4/Assets/Scripts/SpawnManager.cs
@@ -5,23 +5,38 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerUpPrefab;
     private float spawnRange = 9.0f;
+    public int enemycount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        float spawnPosX = Random.Range(-spawnRange, spawnRange);
-        float spawnPosZ = Random.Range(-spawnRange, spawnRange);
+        SpawnEnemyWave(waveNumber);
+        Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
 
-        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-
-        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        enemycount = FindObjectsOfType<Enemy>().Length;
+
+        if (enemycount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerUpPrefab, GenerateSpawnPosition(), powerUpPrefab.transform.rotation);
+        }
+    }
+
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
     }
     
     private Vector3 GenerateSpawnPosition()
